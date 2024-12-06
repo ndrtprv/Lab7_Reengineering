1949a5497df2e649db0334c73fcb986f34f33f6e
ndrtprv <ndrtprv@gmail.com>
2024-12-06 11:10:23 +0200

    Task 3.5.

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -96,12 +96,9 @@ public class ShoppingCart {
         // column max length
         int[] width = new int[]{0, 0, 0, 0, 0, 0};
         for (String[] line : lines)
-            for (int i = 0; i < line.length; i++)
-                width[i] = (int) Math.max(width[i], line[i].length());
-        for (int i = 0; i < header.length; i++)
-            width[i] = (int) Math.max(width[i], header[i].length());
-        for (int i = 0; i < footer.length; i++)
-            width[i] = (int) Math.max(width[i], footer[i].length());
+            adjustColumnWidth(width, line);
+        adjustColumnWidth(width, header);
+        adjustColumnWidth(width, footer);
 
         // line length
         int lineLength = width.length - 1;
@@ -110,33 +107,36 @@ public class ShoppingCart {
         StringBuilder sb = new StringBuilder();
 
         // header
-        for (int i = 0; i < header.length; i++)
-            appendFormatted(sb, header[i], align[i], width[i]);
-        sb.append("\n");
+        appendFormattedLine(sb, header, align, width, true);
 
         // separator
-        for (int i = 0; i < lineLength; i++)
-            sb.append("-");
-        sb.append("\n");
+        appendSeparator(sb, lineLength);
 
         // lines
         for (String[] line : lines) {
-            for (int i = 0; i < line.length; i++)
-                appendFormatted(sb, line[i], align[i], width[i]);
-            sb.append("\n");
-        }
-
-        if (lines.size() > 0) {
-            // separator
-            for (int i = 0; i < lineLength; i++)
-                sb.append("-");
-            sb.append("\n");
+            appendSeparator(sb, lineLength);
         }
 
         // footer
-        for (int i = 0; i < footer.length; i++)
-            appendFormatted(sb, footer[i], align[i], width[i]);
+        appendFormattedLine(sb, footer, align, width, false);
         return sb.toString();
+    }
+
+    private void appendSeparator(StringBuilder sb, int lineLength) {
+        for (int i = 0; i < lineLength; i++)
+            sb.append("\n");
+    }
+
+    private void appendFormattedLine(StringBuilder sb, String[] line, int[] align, int[] width, Boolean newLine) {
+        for (int i = 0; i < line.length; i++)
+            appendFormatted(sb, line[i], align[i], width[i]);
+        if (newLine)
+            sb.append("\n");
+    }
+
+    private void adjustColumnWidth(int[] width, String[] columns) {
+        for (int i = 0; i < columns.length; i++)
+            width[i] = (int) Math.max(width[i], columns[i].length());
     }
 
     // --- private section -----------------------------------------------------
